96b8bb5925d42904a32beda5a9dc046b90eb09cd
nimr0dwastaken <41329362+nimr0dwastaken@users.noreply.github.com>
2019-11-27 09:42:09 +0100

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -4,13 +4,13 @@
  * @returns {getParam.params|params}
  */
 function getParam(name) {
-    var url = new URL(window.location.href), p = url.search, parts = {}, params = {};
-    p = p.substr(1);
-    p.split('&').forEach((e, i) => {
-        parts = e.split('=');
-        params[parts[0]] = parts[1];
-    });
-    return params[name];
+	var url = new URL(window.location.href), p = url.search, parts = {}, params = {};
+	p = p.substr(1);
+	p.split('&').forEach((e, i) => {
+		parts = e.split('=');
+		params[parts[0]] = parts[1];
+	});
+	return params[name];
 }
 
 /**
@@ -21,79 +21,79 @@ function getParam(name) {
  * @returns {void}
  */
 function setParam(name, value) {
-    var u = new URL(window.location.href), p = u.search, b = u.origin + u.pathname, a = {}, s = {}, np = [], nq = '';
-    if (p === "") {
-        nq = '?' + name + '=' + value;
-    } else if (p.indexOf('?') === 0) {
-        p = p.substr(1);
-        p.split('&').forEach((e, i) => {
-            a = e.split('=');
-            s[a[0]] = a[1];
-        });
-    }
-    s[name] = value;
-    for (const [i, [k, v]] of Object.entries(Object.entries(s))) {
-        np[i] = k + '=' + v;
-    }
-    np = np.filter(function (el) {
-        return el;
-    });
-    nq = (np.length === 0 ? '' : '?' + np.join('&'));
-    history.pushState(null, null, b + nq);
+	var u = new URL(window.location.href), p = u.search, b = u.origin + u.pathname, a = {}, s = {}, np = [], nq = '';
+	if (p === "") {
+		nq = '?' + name + '=' + value;
+	} else if (p.indexOf('?') === 0) {
+		p = p.substr(1);
+		p.split('&').forEach((e, i) => {
+			a = e.split('=');
+			s[a[0]] = a[1];
+		});
+	}
+	s[name] = value;
+	for (const [i, [k, v]] of Object.entries(Object.entries(s))) {
+		np[i] = k + '=' + v;
+	}
+	np = np.filter(function (el) {
+		return el;
+	});
+	nq = (np.length === 0 ? '' : '?' + np.join('&'));
+	history.pushState(null, null, b + nq);
 }
 
 // Set cookie value
 function setCookie(n,v) {
-    document.cookie = n+'='+v;
-    return true;
+	document.cookie = n+'='+v;
+	return true;
 }
 
 // Get cookie value
 function getCookie(n) {
-    var a = document.cookie.split('; '), b = {}, c;
-    a.forEach(function (e, i) {
-        c = e.split('=')[1];
-        b[e.split('=')[0]] = c === 'false' ? false : (c === 'true' ? true : c);
-    });
-    return b[n];
+	var a = document.cookie.split('; '), b = {}, c;
+	a.forEach(function (e, i) {
+		c = e.split('=')[1];
+		b[e.split('=')[0]] = c === 'false' ? false : (c === 'true' ? true : c);
+	});
+	return b[n];
 }
 // Easy date formatting
 function myTime(date, addLeadingZero = true) {
-    var d = new Date(date);
-    if (d === 'Invalid Date') {return false;}
-    var dd = d.getDate(), mm = d.getMonth() + 1, yyyy = d.getFullYear(), h = d.getHours(), i = d.getMinutes(), s = d.getSeconds();
-    if (addLeadingZero === true) {
-        if (dd < 10) {dd = '0' + dd;}
-        if (mm < 10) {mm = '0' + mm;}
-        if (h < 10) {h = '0' + h;}
-        if (i < 10) {i = '0' + i;}
-        if (s < 10) {s = '0' + s;}
-    }
-    var ret = {
-        day: dd,
-        month: mm,
-        year: yyyy,
-        hour: h,
-        minute: i,
-        second: s
-    };
-    return ret;
+	var d = new Date(date);
+	if (d === 'Invalid Date') {return false;}
+	var dd = d.getDate(), mm = d.getMonth() + 1, yyyy = d.getFullYear(), h = d.getHours(), i = d.getMinutes(), s = d.getSeconds();
+	if (addLeadingZero === true) {
+		if (dd < 10) {dd = '0' + dd;}
+		if (mm < 10) {mm = '0' + mm;}
+		if (h < 10) {h = '0' + h;}
+		if (i < 10) {i = '0' + i;}
+		if (s < 10) {s = '0' + s;}
+	}
+	var ret = {
+		day: dd,
+		month: mm,
+		year: yyyy,
+		hour: h,
+		minute: i,
+		second: s
+	};
+	return ret;
 }
 
 // Duh
 function isInt(n) {
-    return Number(n) === n && n % 1 === 0;
+	return Number(n) === n && n % 1 === 0;
 }
 // Duh
 function isFloat(n) {
-    return Number(n) === n && n % 1 !== 0;
+	return Number(n) === n && n % 1 !== 0;
 }
 // Duh
 function randomInt(min, max) {
-    return Math.floor(min + Math.random() * (max + 1 - min))
+	return Math.floor(min + Math.random() * (max + 1 - min))
 }
 
 // Reload page after 'timeout' miliseconds
 function windowReload(timeout = 0) {
-    setTimeout(window.location.reload.bind(window.location), timeout);
+	setTimeout(window.location.reload.bind(window.location), timeout);
 }
