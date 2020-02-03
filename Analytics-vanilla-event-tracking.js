3b7506a6d834f6b2d0e07a327c94a15238478aa5
nimr0dwastaken <41329362+nimr0dwastaken@users.noreply.github.com>
2020-02-03 10:22:36 +0100

    Create Analytics-vanilla-event-tracking.js

---
+++ Analytics-vanilla-event-tracking.js
@@ -0,0 +1,38 @@
+    // DOC ready  
+    document.addEventListener('DOMContentLoaded', function () {
+
+        // Elements to track    
+        let evtTracker = [
+            {
+                category: 'B2B',
+                selector: '[href="/shop/b2blogin.html"]',
+                label: 'B2B login click',
+            },
+            {
+                category: 'Kontakt',
+                selector: '[href="tel:+4571993159"]',
+                label: 'Tlf click',
+            },
+            {
+                category: 'Kontakt',
+                selector: '[href="mailto:info@allaboutyou.dk"]',
+                label: 'Mail click',
+            },
+        ];
+
+
+        // Query each element selector
+        evtTracker.forEach(evt => {
+            // Add event listener to each element (1 selector can have multiple elements)
+            document.querySelectorAll(evt.selector).forEach(element => {
+                element.addEventListener('click', e => {
+                    
+                    gtag('event', e.type, {
+                        'event_category': evt.category,
+                        'event_label': evt.label,
+                    });
+
+                }, false);
+            });
+        });
+    }, false);
